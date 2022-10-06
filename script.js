6e65a62ba7632cab0a5c58b10faebaa0e506b663
SimonjShurety <simonjshurety@gmail.com>
2022-10-05 21:07:55 -0300

    Refactored solution

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ For example, if we run 9119 through the function, 811181 will come out, because 
 
 Note: The function accepts an integer and returns an integer */
 
-function squareDigits(num){
-  let str = num.toString()
-  let arr = num.toString().split('')
-  let sqr = arr.map(x => x ** 2)
-  let ans = sqr.join('')
-   return ans
-  }
-  squareDigits(9119);
+/* function squareDigits(num) {
+  let str = num.toString();
+  let arr = num.toString().split("");
+  let sqr = arr.map((x) => x ** 2);
+  let ans = sqr.join("");
+  return ans;
+}
+squareDigits(9119); */
+
+const squareDigits = (num) =>
+  String(num)
+    .split("")
+    .map((x) => x ** 2)
+    .join("");
+
+squareDigits(9119);
