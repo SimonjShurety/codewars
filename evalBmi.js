a573446d862000f29b7b8f2caedcc72018e6da82
SimonjShurety <simonjshurety@gmail.com>
2022-10-09 21:58:39 -0300

    Refactored ternary operator version

--- evalBmi.js
+++ evalBmi.js
@@ -1,10 +1,22 @@
+// function bmi(weight, height) {
+//   const personBmi = (bmi = weight / height ** 2);
+//   if (personBmi <= 18.5) return "Underweight";
+
+//   if (personBmi <= 25.0) return "Normal";
+
+//   if (personBmi <= 30.0) return "Overweight";
+
+//   if (personBmi > 30.0) return "Obese";
+// }
+
 function bmi(weight, height) {
-  const personBmi = (bmi = weight / height ** 2);
-  if (personBmi <= 18.5) return "Underweight";
+  const x = (bmi = weight / height ** 2);
 
-  if (personBmi <= 25.0) return "Normal";
-
-  if (personBmi <= 30.0) return "Overweight";
-
-  if (personBmi > 30.0) return "Obese";
+  return x <= 18.5
+    ? "Underweight"
+    : x <= 25.0
+    ? "Normal"
+    : x <= 30.0
+    ? "Overweight"
+    : "Obese";
 }
